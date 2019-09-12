99458d1605dbd647cbdd762575c79bff161d46e7
Jackson Perry <jpperry@wpi.edu>
2019-09-12 18:42:38 -0400

    Update LinAlg_A1.R

--- LinAlg_A1.R
+++ LinAlg_A1.R
@@ -19,7 +19,7 @@ text(leuk$V675 ~ leuk$V45, labels=rownames(leuk),data=leuk, cex=0.5, font=2)
 pcaOut = prcomp(leuk[,1:5000], scale = F)
 
 #plot again
-plot(pcaOut$x[,1], pcaOut$x[,2], col = leuk$V5001, pch=19, cex=2,)
+plot(pcaOut$x[,1], pcaOut$x[,2], col = leuk$V5001, pch=19, cex=2)
 text(pcaOut$x[,2] ~ pcaOut$x[,1], labels=rownames(leuk), data=leuk, cex=0.5, font=2)
 legend(40000,-25000, legend=c("AML","ALL-B","ALL-T"), col = c("#4DAF4A","#EF1A1C","#377EB8"), pch=19, cex=1)#blue is allt, aml is green)
 
@@ -43,6 +43,39 @@ legend(40000,-25000, legend=c("AML","ALL-B","ALL-T"), col = c("#4DAF4A","#EF1A1C
 #scaled was way worse
 ###########################
 
+###########################
+# conclusion: 
+# we used principal component analysis on unscaled data
+# 2 PCs explain almost 36% of the variance in the type of leukemia
+# point 22 appears to be mislabeled
+###########################
+
+#get the mean of each group
+new <- cbind(pcaOut$x[,1],pcaOut$x[,2],leuk$V5001)
+
+reds <- new[(new[,3]==1),]
+blues <- new[(new[,3]==2),]
+greens <- new[(new[,3]==3),]
+
+meanR <- c(mean(reds[,1]), mean(reds[,2]))
+meanB <- c(mean(blues[,1]), mean(blues[,2]))
+meanG <- c(mean(greens[,1]), mean(greens[,2]))
+
+#look at coordinates of point 22
+new[19,]
+
+#calculate distance to each
+dtoB <- sqrt((new[19,1]-meanB[1])**2 + (new[19,2]-meanB[2])**2)
+dtoG <- sqrt((new[19,1]-meanG[1])**2 + (new[19,2]-meanG[2])**2)
+
+###########################
+# conclusion:
+# point 22 is much closer to the center of the blue cluster than 
+# to the center of the green cluster
+
+###########################
+
+#IGNORE EVERYTHING BELOW#
 #here is my idea: we can compute the variance for each group as is
 #find the change in color that lowers the variance the most
 #1-red, 2-blue, 3-green
@@ -97,8 +130,42 @@ varG <- c(var(greens[,1]), var(greens[,2]))
 
 tracker <- cbind(tracker, c(varR,varB,varG))
 
+#make 29 blue
+new <- cbind(pcaOut$x[,1],pcaOut$x[,2],leuk$V5001)
+new[2,3] <- 2
+
+reds <- new[(new[,3]==1),]
+blues <- new[(new[,3]==2),]
+greens <- new[(new[,3]==3),]
+
+meanR <- c(mean(reds[,1]), mean(reds[,2]))
+meanB <- c(mean(blues[,1]), mean(blues[,2]))
+meanG <- c(mean(greens[,1]), mean(greens[,2]))
+varR <- c(var(reds[,1]), var(reds[,2]))
+varB <- c(var(blues[,1]), var(blues[,2]))
+varG <- c(var(greens[,1]), var(greens[,2]))
+
+tracker <- cbind(tracker, c(varR,varB,varG))
+
+#make 20 green
+new <- cbind(pcaOut$x[,1],pcaOut$x[,2],leuk$V5001)
+new[10,3] <- 3
+
+reds <- new[(new[,3]==1),]
+blues <- new[(new[,3]==2),]
+greens <- new[(new[,3]==3),]
+
+meanR <- c(mean(reds[,1]), mean(reds[,2]))
+meanB <- c(mean(blues[,1]), mean(blues[,2]))
+meanG <- c(mean(greens[,1]), mean(greens[,2]))
+varR <- c(var(reds[,1]), var(reds[,2]))
+varB <- c(var(blues[,1]), var(blues[,2]))
+varG <- c(var(greens[,1]), var(greens[,2]))
+
+tracker <- cbind(tracker, c(varR,varB,varG))
+
 ##############################
 #it looks like changing obs 22 to blue reduced group 
 #variance the most, so I think this is the mislabeled point
 ##############################
-
+tracker[7,] <- sum(tracker[1:6,])
